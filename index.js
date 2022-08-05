336416175852c6876ee32209db7899419086b9d3
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-08-04 20:23:06 -0700

    made init fuction and made bare bones readme and licence

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Included packages needed for this application
-const GenMark = require("./utils/generateMarkdown");
+const genMark = require("./utils/generateMarkdown");
 const inquirer = require('inquirer');
 const fs = require("fs");
 //Created an array of questions for user input
@@ -10,38 +10,38 @@ const questions = [
         message: "What is your readme title?",
     },
     {
-        name: "Description",
+        name: "description",
         type: "input",
         message: "What is your project description?"
     },
     {
-        name: "Installation",
+        name: "installation",
         type: "input",
         message: "What is the installation instructions?"
     },
     {
-        name: "Usage",
+        name: "usage",
         type: "input",
         message: "What is the usage information?"
     },
     {
-        name: "License",
-        type: "checkbox",
+        name: "license",
+        type: "list",
         message: "What is the license?",
-        choices: ["Engineer", "Intern", "Manager"]
+        choices: ["Apache 2.0 License", "CC0", "GNU GPL v3", "The Hippocratic License 2.1", "The MIT License", "Mozilla Public License 2.0", "The Do What the Fuck You Want to Public License"]
     },
     {
-        name: "Contributing",
+        name: "contributing",
         type: "input",
         message: "How do people contribute?"
     },
     {
-        name: "Tests",
+        name: "tests",
         type: "input",
         message: "How do people test?"
     },
     {
-        name: "Questions",
+        name: "questions",
         type: "input",
         message: "How would they ask you questions?"
     },
@@ -63,7 +63,20 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            replaceLicense(answers);
+        })
+        .catch((err) => {
+            if (err.isTtyError) {
+                console.log("Prompt couldn't be rendered in the current environment!");
+            } else {
+                console.log("Something went wrong!");
+            }
+        });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,133 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "Apache 2.0 License":
+      // code block
+      license = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      break;
+    case "CC0":
+      // code block
+      license = "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)";
+      break;
+    case "GNU GPL v3":
+      // code block
+      license = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "The Hippocratic License 2.1":
+      // code block
+      license = "[![License: Hippocratic 2.1](https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg)](https://firstdonoharm.dev)";
+      break;
+    case "The MIT License":
+      // code block
+      license = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      break;
+    case "Mozilla Public License 2.0":
+      // code block
+      license = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+      break;
+    case "The Do What the Fuck You Want to Public License":
+      // code block
+      license = "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)";
+      break;
+    default:
+      // code block
+      license = "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "Apache 2.0 License":
+      // code block
+      license = "https://opensource.org/licenses/Apache-2.0";
+      break;
+    case "CC0":
+      // code block
+      license = "http://creativecommons.org/publicdomain/zero/1.0/";
+      break;
+    case "GNU GPL v3":
+      // code block
+      license = "https://www.gnu.org/licenses/gpl-3.0";
+      break;
+    case "The Hippocratic License 2.1":
+      // code block
+      license = "https://firstdonoharm.dev";
+      break;
+    case "The MIT License":
+      // code block
+      license = "https://opensource.org/licenses/MIT";
+      break;
+    case "Mozilla Public License 2.0":
+      // code block
+      license = "https://opensource.org/licenses/MPL-2.0";
+      break;
+    case "The Do What the Fuck You Want to Public License":
+      // code block
+      license = "http://www.wtfpl.net/about/";
+      break;
+    default:
+      // code block
+      license = "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  lBagde = renderLicenseBadge(license);
+  lLink = renderLicenseLink(license);
+  if(!license) {
+    return "";
+  } else {
+    return `### License
+    
+    ${lBagde}
+    ${lLink}`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ## Table of Contents
+  [Example](#description)
+  [Example2](#dnstallation)
+  [Third Example](#usage)
+  [Fourth Example](#contributing)
+  [Fourth Example](#tests)
+  [Fourth Example](#questions)
+
+  ## Description
+
+  ${data.description}
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+
+  ${data.questions}
+
+  You can send me any additional questions at my email ${data.email}.
+
+  My [GitHub](https://github.com/${data.github})
 
 `;
 }
